713175a957d87eabdb88c4506d3beadc3c5b2bf8
Julia Stamborovski <juliashizuko66@gmail.com>
2023-05-18 16:01:36 -0300

    scraping click

--- idwall/scraping/web_scraper.py
+++ idwall/scraping/web_scraper.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 def scrape_fbi_website():
     driver_path = ChromeDriverManager().install()
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--headless")
     service = webdriver.chrome.service.Service(driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get('https://www.fbi.gov/wanted/cyber')
@@ -23,9 +23,22 @@ def scrape_fbi_website():
     extracted_data = []
 
     for person in wanted_persons:
-        name_wanted = person.find_element(By.CLASS_NAME, 'title').text
-        
+        title_element = person.find_element(By.CSS_SELECTOR, 'h3 > a')
+        name_wanted = title_element.text
 
+        # scroll
+        driver.execute_script("arguments[0].scrollIntoView(true);", title_element)
+        sleep(1)
+
+        # click
+        driver.execute_script("arguments[0].click();", title_element)
+        sleep(2)
+
+        #  os dados dos detalhes da pessoa procurada aqui
+       
+        # back iterando
+        driver.execute_script("window.history.go(-1)")
+        sleep(2)
         data = {
             'name': name_wanted,
            
