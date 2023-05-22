c37ad09841e86d2775fe081cbca46f71035a5a19
Julia Stamborovski <juliashizuko66@gmail.com>
2023-05-22 19:51:21 -0300

    ajuste

--- scrapingIdwall/urls.py
+++ scrapingIdwall/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path #include
 from idwall.views import home 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('idwall.urls')),
+    #path('api/', include('idwall.urls')),
     path("", home, name='home'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
